2a85f24860c4201416e526b305644ba651b04b84
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-11 01:41:22 -0600

    snake

--- input.js
+++ input.js
@@ -1,21 +1,39 @@
-const setupInput = function () {
+const readline = require("readline");
+
+let connection; // Stores the active TCP connection object
+
+const setupInput = (conn) => {
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
   stdin.resume();
 
+  const rl = readline.createInterface({
+    input: stdin,
+    output: process.stdout,
+  });
+
   // Register event listener for stdin data
-  stdin.on("data", handleUserInput);
+  stdin.on("data", (key) => handleUserInput(key, rl));
 
   return stdin;
 };
 
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
+const handleUserInput = function (key, rl) {
+  if (key === "\u0003") {
     // Check for Ctrl + C input (termination)
     process.exit();
+  } else if (key === "w") {
+    connection.write("Move: up");
+  } else if (key === "a") {
+    connection.write("Move: left");
+  } else if (key === "s") {
+    connection.write("Move: down");
+  } else if (key === "d") {
+    connection.write("Move: right");
   }
-  // Your code here for handling other input
 };
 
 module.exports = { setupInput };
--- play.js
+++ play.js
@@ -1,13 +1,14 @@
 const net = require("net");
+const { setupInput } = require("./input");
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
     port: 50541 // PORT number here,
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
 
   conn.on("connect", () => {
@@ -15,40 +16,19 @@ const connect = function () {
     // Send the name to the server
     conn.write("Name: ABB");
 
-    // additional logic after connection is established
+    // Additional logic after connection is established
   });
 
   conn.on("data", (data) => {
     console.log("Received data:", data);
-    // additional logic to handle incoming data
+    // Additional logic to handle incoming data
   });
 
   return conn;
-};
-
-// setup interface to handle user input from stdin
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  // Register event listener for stdin data
-  stdin.on("data", handleUserInput);
-
-  return stdin;
-};
-
-const handleUserInput = function (data) {
-  if (data === '\u0003') {
-    // Check for Ctrl + C input (termination)
-    process.exit();
-  }
-  // Your code here for handling other input
 };
 
 console.log("Connecting ...");
 const connection = connect();
 
 // Call the setupInput function
-setupInput();
+setupInput(connection);
