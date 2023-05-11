490f54088cdaf909cc32d11f399e69d687b3e315
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-11 00:58:50 -0600

    updated

--- play.js
+++ play.js
@@ -26,5 +26,29 @@ const connect = function () {
   return conn;
 };
 
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  // Register event listener for stdin data
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    // Check for Ctrl + C input (termination)
+    process.exit();
+  }
+  // Your code here for handling other input
+};
+
 console.log("Connecting ...");
-connect();
+const connection = connect();
+
+// Call the setupInput function
+setupInput();
