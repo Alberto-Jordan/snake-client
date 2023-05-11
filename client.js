2059fb6e17bba5ce8aca49188a2a8d2dc4f4b72c
Alberto-Jordan <alberto.jordan408@gmail.com>
2023-05-11 01:52:55 -0600

    updated

--- client.js
+++ client.js
@@ -1,16 +1,31 @@
 const net = require("net");
+const { IP, PORT } = require("./constants");
 
-// establishes a connection with the game server
+// Establishes a connection with the game server
 const connect = function () {
   const conn = net.createConnection({
-    host:"165.227.47.243", // IP address here,
-    port: 50541 // PORT number here,
+    host: IP,
+    port: PORT
   });
 
-  // interpret incoming data as text
+  // Interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    // Send the name to the server
+    conn.write("Name: ABB");
+
+    // Additional logic after connection is established
+  });
+
+  conn.on("data", (data) => {
+    console.log("Received data:", data);
+    // Additional logic to handle incoming data
+  });
 
   return conn;
 };
 
-module.exports = connect;
+console.log("Connecting ...");
+connect();
